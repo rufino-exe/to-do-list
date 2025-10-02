eb0fcf36e8857149409f9f2ad75c3289e3363e43
rufino-exe <rafaelceschirufino@gmail.com>
2025-10-02 19:14:28 -0300

    Adição do return para demonstração do ToDo.

--- src/App.js
+++ src/App.js
@@ -13,5 +13,32 @@ export default function App(){
     const newToDo = {id:Date.now(), Text: newTask}
     
     setToDos([...toDos,newToDo])
+
+    setNewTask("")
   }
+  // função que remove uma tarefa pelo id
+  const removeTask = (id) =>{
+    setToDos(toDos.filter((toDo)=> toDo.id !== id))
+  }
+  return(
+    <div>
+      <h1>Minha ToDo List</h1>
+      <div>
+        <input 
+        type="text" 
+        placeholder="Digite uma nova tarefa!" 
+        value={newTask}
+        onChange={(e)=>setNewTask(e.target.value)}
+        style={styles.input}
+        />
+        <button onClick={addTask} style={styles.button}>
+          Adicionar
+        </button>
+        <div>
+          <ToDoList toDos={toDos} removeTask={removeTask}/>
+        </div>
+      </div>
+    </div>
+  )
 }
+
